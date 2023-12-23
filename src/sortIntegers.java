4c03d8c52f2aca4be50e93a01ab0dc484a3240ea
ameeraattiah <amaattiah@effat.edu.sa>
2023-12-23 12:34:06 +0300

    sorting integers

---
+++ src/sortIntegers.java
@@ -0,0 +1,36 @@
+public class sortIntegers {
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            // Swap the found maximum element with the element at index i
+            int temp = array[maxIndex];
+            array[maxIndex]= array[i];
+            array[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        selectionSort(array);
+
+        System.out.println("Array in non-increasing order:");
+        printArray(array);
+    }
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
